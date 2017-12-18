bb66fb8094c69eabf46977d0250ca4566327157c
Emmanuel Ramos <emmanuel.ramos@dextra-sw.com>
2017-12-18 13:49:54 -0200

    Adding Jedis pool

---
+++ src/main/java/com/api/config/CacheConfig.java
@@ -0,0 +1,48 @@
+package com.api.config;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private Integer redisPort;
+
+    @Value("${redis.timeout}")
+    private Integer redisTimeout;
+
+    @Value("${redis.password}")
+    private String redisPassword;
+
+    private JedisPoolConfig buildPoolConfig() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+        return poolConfig;
+    }
+
+    @Bean
+    public JedisPool jedisPool() {
+        return new JedisPool(buildPoolConfig(),
+                redisHost, redisPort, redisTimeout,
+                StringUtils.isEmpty(redisPassword) ? null : redisPassword);
+    }
+}
